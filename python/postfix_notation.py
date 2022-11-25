1a7a6cec28c366992156e7329a023066c3420b60
myandue <97776790+myandue@users.noreply.github.com>
2022-11-25 15:21:10 +0900

    22.11.25
    solving a 'postfix notation' problem

--- python/postfix_notation.py
+++ python/postfix_notation.py
@@ -4,18 +4,35 @@ s = list(str(input()))
 answer=[]
 stack=[]
 
-for i in range(len(s)):
+for i in range(len(s)-1):
     if s[i].isalpha():
         answer.append(s[i])
+    elif s[i]=='(':
+        stack.append(s[i])
+    elif s[i]==')':
+        while len(stack)!=0:
+            x=stack.pop()
+            if x=='(':
+                break
+            answer.append(x)
     else:
         l=len(stack)
-        if l==0 or stack[l-1]=='(' or s[i]=='*' or s[i]=='/' or s[i]=='(':
-            pass
+        if s[i]=='*' or s[i]=='/':
+            if l!=0:
+                if stack[l-1] =='*' or stack[l-1]=='/':
+                    answer.append(stack.pop())
         else:
-            for j in range(l):
-                x=stack.pop()
-                if x!='(' and x!=')':
+            if l!=0:
+                while len(stack)!=0:
+                    x=stack.pop()
+                    if x=='(':
+                        stack.append(x)
+                        break
                     answer.append(x)
         stack.append(s[i])
 
+l=len(stack)
+if l!=0:
+    for i in range(l):
+        answer.append(stack.pop())
 print(''.join(answer))
